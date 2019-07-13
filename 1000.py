cae309bae25421c15e21b9ab5374831a94a7d61a
ystaeyoon113 <52200017+ystaeyoon113@users.noreply.github.com>
2019-07-13 22:26:39 +0900

    Add files via upload

---
+++ 1000.py
@@ -0,0 +1,8 @@
+# Addition program
+# add inserted two integers
+
+# ex) 1 2 >>>> 3
+
+user = input()
+num_list = user.split()
+print(int(num_list[0]) + int(num_list[1]))
---
+++ 1001.py
@@ -0,0 +1,8 @@
+# Subtraction program
+# subtract inserted two integers
+
+# ex) 3 2 >>>> 1
+
+user = input()
+num_list = user.split()
+print(int(num_list[0]) - int(num_list[1]))
